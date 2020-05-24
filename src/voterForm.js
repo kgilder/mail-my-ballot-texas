ee1254eb51863b378a2cc0d76ae2b4553a89a058
Ken Gildersleeve <kg.sleeve@gmail.com>
2020-05-24 14:04:09 -0500

    Updated with better submit handling, template for civic API access

--- src/voterForm.js
+++ src/voterForm.js
@@ -6,7 +6,7 @@ import './Logo.css';
 console.log(logo);
 
 function Header() {
-  return <img src={logo} alt="Logo" class="center"/>;
+  return <img src={logo} alt="Logo" className="center"/>;
 }
 
 class TextInput extends React.Component {
@@ -17,7 +17,8 @@ constructor(props) {
       active: (props.locked && props.active) || false,
       value: props.value || "",
       error: props.error || "",
-      label: props.label || "Label"
+      label: props.label || "Label",
+      name:  props.name || "input"
     };
   }
 
@@ -33,7 +34,7 @@ constructor(props) {
   }
 
   render() {
-    const { active, value, error, label } = this.state;
+    const { active, value, error, label, name } = this.state;
     const { predicted, locked } = this.props;
     const fieldClassName = `field ${(locked ? active : active || value) &&
       "active"} ${locked && !active && "locked"}`;
@@ -46,6 +47,7 @@ constructor(props) {
           predicted.includes(value) && <p className="predicted">{predicted}</p>}
         <input
           id={1}
+          name={name}
           type="text"
           value={value}
           placeholder={label}
@@ -62,32 +64,48 @@ constructor(props) {
   }
 }
 
-
-function buildMail(form) {
-  var str = "mailto:dana.debeauvoir@traviscountytx.gov?subject=Request for Application for Ballot by Mail&body=";
-  str += "I am writing this email to request to request an application for ballot by mail for the upcoming election.%0D%0A%0D%0A";
-  str += "My name is Kenneth Gildersleeve and my home address is:%0D%0A";
-  str += "1300 Newning Ave Apt 207%0D%0A";
-  str += "Austin, TX 78704%0D%0A%0D%0A";
-  str += "Thank you very much,%0D%0AKenneth Gildersleeve%0D%0A";
-  window.open(str);
-}
-
 class VoterForm extends React.Component {
+  
+  componentDidMount(){
+    fetch('https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDATZ2arFUsfIjTF8CkufwKdUjH7fM5eVg&address=1300%20Newning%20Ave.%20Austin%20TX&electionId=2000').then(function(response) {
+      return response.json(); 
+    }).then(function(j) {
+      console.log(j);
+      console.log(j.normalizedInput.line1 + ' ' + j.normalizedInput.city + ', ' + j.normalizedInput.state); 
+      console.log(j.state[0].local_jurisdiction.name);
+    }).catch(console.log);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const fname = data.get('fname');
+    const lname = data.get('lname');
+    const street = data.get('street');
+    const city = data.get('city');
+    const zip = data.get('zip');
+    console.log(data);
+    var str = "mailto:dana.debeauvoir@traviscountytx.gov?subject=Request for Application for Ballot by Mail&body=";
+    str += "I am writing this email to request to request an application for ballot by mail for the upcoming election.%0D%0A%0D%0A";
+    str += "My name is " + fname + " " + lname + " and my home address is:%0D%0A";
+    str += street + "%0D%0A" + city + ", TX " + zip + "%0D%0A%0D%0A";
+    str += "Thank you very much,%0D%0A" + fname + " " + lname + "%0D%0A";
+    window.open(str);
+  }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <div className="Header">
           <Header />
         </div>
         <div className="Form">
-        <form action={buildMail(this)} method="get" enctype="text/plain">
-          <TextInput label="First Name" />
-          <TextInput label="Last Name" />
-          <TextInput label="Street" />
-          <TextInput label="City" />
-          <TextInput label="Zip Code" />
+        <form onSubmit={this.handleSubmit} method="get" encType="text/plain">
+          <TextInput label="First Name" name="fname"/>
+          <TextInput label="Last Name" name="lname"/>
+          <TextInput label="Street" name="street"/>
+          <TextInput label="City" name="city"/>
+          <TextInput label="Zip Code" name="zip"/>
           <div className="field">
             <input type="submit" value="Submit" />
           </div>
