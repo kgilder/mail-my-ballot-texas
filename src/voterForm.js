f2b809fccf5d0806e43bf5b189d3385ea0a8f399
Ken Gildersleeve <kg.sleeve@gmail.com>
2020-05-25 12:58:46 -0500

    Cleaned up request to google civic API

--- src/voterForm.js
+++ src/voterForm.js
@@ -10,7 +10,7 @@ function Header() {
 }
 
 class TextInput extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -72,35 +72,91 @@ function importCountyElectionInformation(){
   //console.log(result);
 }
 
+async function getCivicInfo(voter_info) {
+  const url_fname = encodeURIComponent(voter_info.fname);
+  const url_lname = encodeURIComponent(voter_info.lname);
+  const url_street = encodeURIComponent(voter_info.street);
+  const url_city = encodeURIComponent(voter_info.city);
+  const url_zip = encodeURIComponent(voter_info.zip);
+  const civic_api_request = 'https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDATZ2arFUsfIjTF8CkufwKdUjH7fM5eVg&address=' + url_street + '%20' + url_city + '%20' + voter_info.state + '&electionId=2000'; 
+  console.log('Get Civic Info');
+  console.log(civic_api_request);
+  const civic_info = await fetch(civic_api_request).then(res => res.json()).catch(error => console.error('Error:', error)).then(function(response) {
+    console.log('Success');
+    return response;
+  });
+  console.log('Info:',civic_info); 
+  return civic_info.state[0].local_jurisdiction.name;
+}
+
+async function getVoterInfo(voterData) {
+  const data = await fetch('https://teamrv-mvp.sos.texas.gov/MVP/voterDetails.do', 
+    { method: 'POST', 
+      mode: 'no-cors', 
+      redirect: 'follow',
+      body: JSON.stringify(voterData),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(res => res.json()).catch(error => console.error('Error:', error)).then(response => console.log('Success:',response));
+}
+
 class VoterForm extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.voter_info = {
+      selType: 'lfcd',
+      fname: '',
+      lname: '',
+      street: '',
+      city: '',
+      state: 'TX',
+      county: '',
+      dob: '',
+      zip: ''
+    };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   componentDidMount(){
     //{importCountyElectionInformation()};
-    fetch('https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDATZ2arFUsfIjTF8CkufwKdUjH7fM5eVg&address=1300%20Newning%20Ave.%20Austin%20TX&electionId=2000').then(function(response) {
-      return response.json(); 
-    }).then(function(j) {
-      console.log(j);
-      console.log(j.normalizedInput.line1 + ' ' + j.normalizedInput.city + ', ' + j.normalizedInput.state); 
-      console.log(j.state[0].local_jurisdiction.name);
-    }).catch(console.log);
+//    fetch('https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDATZ2arFUsfIjTF8CkufwKdUjH7fM5eVg&address=1300%20Newning%20Ave.%20Austin%20TX&electionId=2000').then(function(response) {
+//      return response.json(); 
+//    }).then(function(j) {
+//      console.log(j);
+//      console.log(j.normalizedInput.line1 + ' ' + j.normalizedInput.city + ', ' + j.normalizedInput.state); 
+//      console.log(j.state[0].local_jurisdiction.name);
+//    }).catch(console.log);
   }
 
   handleSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
-    const fname = data.get('fname');
-    const lname = data.get('lname');
-    const street = data.get('street');
-    const city = data.get('city');
-    const zip = data.get('zip');
-    console.log(data);
-    var str = "mailto:dana.debeauvoir@traviscountytx.gov?subject=Request for Application for Ballot by Mail&body=";
-    str += "I am writing this email to request to request an application for ballot by mail for the upcoming election.%0D%0A%0D%0A";
-    str += "My name is " + fname + " " + lname + " and my home address is:%0D%0A";
-    str += street + "%0D%0A" + city + ", TX " + zip + "%0D%0A%0D%0A";
-    str += "Thank you very much,%0D%0A" + fname + " " + lname + "%0D%0A";
-    window.open(str, "_blank");
+    this.voter_info.fname = data.get('fname');
+    this.voter_info.fname = data.get('lname');
+    this.voter_info.street = data.get('street');
+    this.voter_info.city = data.get('city');
+    this.voter_info.zip = data.get('zip');
+
+    this.voter_info.county = getCivicInfo(this.voter_info);
+    console.log('County:',this.voter_info.county);
+    const voterData = {selType: 'lfcd',firstName: this.voter_info.fname, lastName: this.voter_info.lname, county: this.voter_info.county, dob: '11/19/1989', adZip5: this.voter_info.zip};
+    getVoterInfo(voterData);
+///      }).then(function(response) {
+///        
+///        return response.json();
+///      }).then(function(sos_data) {
+///        console.log(sos_data);
+///      }).catch(console.log);
+//    var str = "mailto:dana.debeauvoir@traviscountytx.gov?subject=Request for Application for Ballot by Mail&body=";
+//    str += "I am writing this email to request to request an application for ballot by mail for the upcoming election.%0D%0A%0D%0A";
+//    str += "My name is " + fname + " " + lname + " and my home address is:%0D%0A";
+//    str += street + "%0D%0A" + city + ", TX " + zip + "%0D%0A%0D%0A";
+//    str += "Thank you very much,%0D%0A" + fname + " " + lname + "%0D%0A";
+//    window.open(str, "_blank");
   }
+  
 
   render() {
     return (
